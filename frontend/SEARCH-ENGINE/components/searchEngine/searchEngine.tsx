33dfe647efb2be1d17a25c1e8eb1d35760eb139e
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-06-13 14:02:27 +0200

    add: virtual list

--- frontend/SEARCH-ENGINE/components/searchEngine/searchEngine.tsx
+++ frontend/SEARCH-ENGINE/components/searchEngine/searchEngine.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from "react";
 import { filterSearchResults } from "./searchFilter";
 import { data } from "./mock";
+import { Virtuoso } from "react-virtuoso";
 
 export const SearchEngine = (): React.JSX.Element => {
   const [searchParams, setSearchParams] = useState<string>("");
@@ -21,11 +22,15 @@ export const SearchEngine = (): React.JSX.Element => {
         />
       </div>
       {visibleData.length > 0 ? (
-        visibleData.map((item, index) => (
-          <div key={index} className="p-4 border-b border-gray-200">
-            {item}
-          </div>
-        ))
+        <Virtuoso
+          style={{ height: "calc(100vh - 200px)" }}
+          totalCount={visibleData.length}
+          itemContent={(index) => (
+            <div className="p-4 border-b border-gray-200">
+              {visibleData[index]}
+            </div>
+          )}
+        />
       ) : (
         <p className="text-gray-500">No results found.</p>
       )}
