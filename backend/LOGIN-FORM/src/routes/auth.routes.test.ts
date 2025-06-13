959be108042af02342432ce765076bc66eeb967f
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-06-13 20:02:53 +0200

    misc: remove comment

--- backend/LOGIN-FORM/src/routes/auth.routes.test.ts
+++ backend/LOGIN-FORM/src/routes/auth.routes.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import express from "express";
-import authRoutes from "./auth.routes"; // Tu ruta real con validateData y login
+import authRoutes from "./auth.routes";
 import { StatusCodes } from "http-status-codes";
 
 const app = express();
